7191081665f500caa1948d3da2f4131d840c5cb3
Matthias DOLA <koami.dola@pythagoras-afrique.com>
2015-06-24 11:58:21 +0000

    First commit of the application

---
+++ app/src/main/java/com/example/dola/themeswitcher/ThemeApplication.java
@@ -0,0 +1,11 @@
+package com.example.dola.themeswitcher;
+
+import android.app.Application;
+
+/**
+ * Created by dola on 6/24/15.
+ */
+public class ThemeApplication  extends Application {
+    //App level variable to retain selected spinner value
+    public static int currentPosition;
+}
